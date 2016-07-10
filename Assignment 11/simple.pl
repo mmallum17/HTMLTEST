05d8b083e22ae724d089f9b46b931203da671fc5
Marcus Mallum <marcusmallum@gmail.com>
2016-07-10 10:42:14 -0500

    Update

---
+++ Assignment 11/simple.pl
@@ -0,0 +1,11 @@
+#!/Perl64/bin/perl
+use strict;
+use warnings FATAL => 'all';
+print "Marcus Mallum\n";
+print "mmmallum\n";
+print "Computer Science/Computer Engineering\n";
+print "Lighthouse Pizza / Smashburger\n";
+print "PKI\n";
+print "Faithfully / Journey\n";
+print "I am an avid frolfer and soccer player\n";
+
